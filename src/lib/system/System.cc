c1af3e1689b02aa5b2a54f0b64f8c0c9ec011db6
dnietoc@gmail.com <dnietoc@gmail.com@3b725b2d-4dbd-463f-131a-d5313bb6b835>
2014-04-28 20:58:17 +0000

    automake y main bobo de productorAutos para probar.

---
+++ src/lib/system/System.cc
@@ -0,0 +1,29 @@
+#include <system/System.h>
+
+#include <unistd.h>
+
+SystemErrorException::SystemErrorException () throw ()
+	: number (errno)
+{
+}
+
+SystemErrorException::SystemErrorException (int err) throw ()
+	: number (err)
+{
+}
+
+pid_t System::spawn (const char *file, char *const argv[])
+{
+	pid_t pid;
+
+	pid = fork ();
+	if (pid == 0) {
+		int err = execvp (file, argv);
+		/* TODO: log error */
+		(void) err;
+		_exit (1);
+	}
+	else if (pid == -1)
+		throw SystemErrorException ();
+	return pid;
+}
