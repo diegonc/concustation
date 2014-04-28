87bf6f7f43cb09162379f6fdd5c3877ab122f604
dnietoc@gmail.com <dnietoc@gmail.com@3b725b2d-4dbd-463f-131a-d5313bb6b835>
2014-04-28 20:58:17 +0000

    automake y main bobo de productorAutos para probar.

---
+++ trunk/src/lib/system/System.h
@@ -0,0 +1,38 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include <exception>
+
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+class SystemErrorException : public std::exception
+{
+	private:
+		int number;
+
+	public:
+		SystemErrorException () throw ();
+		SystemErrorException (int err) throw ();
+
+		virtual ~SystemErrorException () throw () { }
+
+		virtual const char *what() const throw()
+		{
+			return strerror (this->number);
+		}
+};
+
+class System
+{
+	public:
+		static pid_t spawn (const char *file, char *const argv[]);
+
+		static void check (int err)
+		{
+			if (err == -1) throw SystemErrorException ();
+		}
+};
+
+#endif
