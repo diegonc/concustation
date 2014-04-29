5b60ea4ef2a0530b8dbfb5d51db92e346723a40a
dnietoc@gmail.com <dnietoc@gmail.com@3b725b2d-4dbd-463f-131a-d5313bb6b835>
2014-04-29 22:27:21 +0000

    Constantes para crear claves para los mecanismos de IPC.

---
+++ src/lib/estacion/constantes.h
@@ -0,0 +1,50 @@
+#ifndef ESTACION_CONSTANTES_H
+#define ESTACION_CONSTANTES_H
+
+namespace estacion {
+	/*
+	 * proj_id pasado a ftok para generar la key correspondiente a la
+	 * memoria compartida utilizada para almacenar la asignacion de tareas.
+	 */
+	const char AREA_TAREAS = 'T';
+
+	/*
+	 * proj_id pasado a ftok para generar la key correspondiente a la
+	 * memoria compartida utilizada para almacenar la caja.
+	 */
+	const char AREA_CAJA = 'C';
+
+	/*
+	 * proj_id pasado a ftok para generar la key correspondiente a la
+	 * memoria compartida utilizada para almacenar la lista de empleados
+	 * libres.
+	 */
+	const char AREA_EMPLEADOS = 'E';
+
+	/*
+	 * proj_id pasado a ftok para generar la key correspondiente a la
+	 * memoria compartida utilizada para almacenar la lista de surtidores
+	 * libres.
+	 */
+	const char AREA_SURTIDORES = 'S';
+
+	/*
+	 * proj_id pasado a ftok para generar la key correspondiente al
+	 * semaforo que protege la lista de empleados libres.
+	 */
+	const char SEM_EMPLEADOS = 'e';
+
+	/*
+	 * proj_id pasado a ftok para generar la key correspondiente al
+	 * semaforo que protege la lista de surtidores libres.
+	 */
+	const char SEM_SURTIDORES = 's';
+
+	/*
+	 * proj_id pasado a ftok para generar la key correspondiente al
+	 * semaforo que protege la caja.
+	 */
+	const char SEM_CAJA = 'c';
+}
+
+#endif // ESTACION_CONSTANTES_H
