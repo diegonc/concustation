828159d2c5582f4080f114bbe39504bf2649ce00
dnietoc@gmail.com <dnietoc@gmail.com@3b725b2d-4dbd-463f-131a-d5313bb6b835>
2014-04-29 21:53:34 +0000

    Clase que encapsula un semaforo de System V.

---
+++ src/lib/system/Semaphore.cc
@@ -0,0 +1,98 @@
+#include <stdexcept>
+
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <iostream>
+
+#include <system/Semaphore.h>
+#include <system/System.h>
+
+namespace {
+	union semun {
+		int val;
+		struct semid_ds *buf;
+		unsigned short  *array;
+	};
+}
+
+Semaphore::Semaphore (IPCName name, int nsems, int flags) : nsems (nsems)
+{
+	key_t token = ftok (name.path, name.index);
+	System::check (token);
+	id = semget (token, nsems, flags);
+	System::check (id);
+	persistent = false;
+}
+
+Semaphore::Semaphore (key_t key, int nsems, int flags) : nsems (nsems)
+{
+	id = semget (key, nsems, flags);
+	System::check (id);
+	persistent = false;
+}
+
+Semaphore::~Semaphore ()
+{
+	if (!persistent) {
+		union semun arg = {0};
+		semctl (id, 0, IPC_RMID, arg);
+	}
+}
+
+void Semaphore::initialize ()
+{
+	for (unsigned short i=0; i < nsems; i++)
+		set (i, 0);
+}
+
+void Semaphore::persist ()
+{
+	persistent = true;
+}
+
+void Semaphore::set (unsigned short idx, short value)
+{
+	union semun arg;
+	arg.val = value;
+	semctl (id, idx, SETVAL, arg);
+}
+
+void Semaphore::wait (unsigned short idx, short value)
+{
+	if (value < 0)
+		throw std::invalid_argument ("Semaphore::wait: value must be positive.");
+
+	struct sembuf ops;
+
+	ops.sem_num = idx;
+	ops.sem_op = static_cast<short>(-value);
+	ops.sem_flg = 0;
+	
+	bool retry;
+	do {
+		retry = false;
+
+		int ret = semop (id, &ops, 1);
+		if (ret == -1) {
+			if (errno == EINTR) {
+				retry = true;
+			} else
+				throw SystemErrorException ();
+		}
+	} while (retry);
+}
+
+void Semaphore::signal (unsigned short idx, short value)
+{
+	if (value < 0)
+		throw std::invalid_argument ("Semaphore::signal: value must be positive.");
+
+	struct sembuf ops;
+
+	ops.sem_num = idx;
+	ops.sem_op = value;
+	ops.sem_flg = 0;
+	System::check (semop (id, &ops, 1));
+}
---
+++ src/lib/system/Semaphore.h
@@ -0,0 +1,30 @@
+#ifndef SEMAPHORE_H
+#define SEMAPHORE_H
+
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+
+#include <system/IPCName.h>
+
+class Semaphore
+{
+	private:
+		bool persistent;
+		int id;
+		const int nsems;
+
+	public:
+		Semaphore (key_t key, int nsems, int flags);
+		Semaphore (IPCName name, int nsems, int flags);
+		virtual ~Semaphore ();
+
+		void persist ();
+		void initialize ();
+
+		void set (unsigned short idx, short value);
+		void wait (unsigned short idx = 0, short value = 1);
+		void signal (unsigned short idx = 0, short value = 1);
+};
+
+#endif
