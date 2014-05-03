57535525a9c1d9dc3385c6df5c8e7bf994af1ff8
dnietoc@gmail.com <dnietoc@gmail.com@3b725b2d-4dbd-463f-131a-d5313bb6b835>
2014-05-03 19:49:22 +0000

    Creación de area de tareas y caja.

--- src/simulacion/main.cc
+++ src/simulacion/main.cc
@@ -2,9 +2,12 @@
 #include <string>
 
 #include <estacion/constantes.h>
+#include <estacion/Tarea.h>
 #include <logging/Logger.h>
 #include <logging/LoggerRegistry.h>
 #include <system/Semaphore.h>
+#include <system/SharedArray.h>
+#include <system/SharedVariable.h>
 
 #include <ArgParser.h>
 
@@ -26,23 +29,32 @@ int main(int argc, char** argv)
 	logger << "Empleados: " << args.empleados () << Logger::endl;
 	logger << "Surtidores: " << args.surtidores () << Logger::endl;
 
-	Semaphore semCaja(
-		IPCName(estacion::PATH_NAME, estacion::SEM_CAJA)
+	Semaphore semCaja (
+		IPCName (estacion::PATH_NAME, estacion::SEM_CAJA)
 		, 1, 0666 | IPC_CREAT | IPC_EXCL);
-	Semaphore semListaSurtidores(
-		IPCName(estacion::PATH_NAME, estacion::SEM_SURTIDORES)
+	Semaphore semListaSurtidores (
+		IPCName (estacion::PATH_NAME, estacion::SEM_SURTIDORES)
 		, 1, 0666 | IPC_CREAT | IPC_EXCL);
-	Semaphore semListaEmpleados(
-		IPCName(estacion::PATH_NAME, estacion::SEM_EMPLEADOS)
+	Semaphore semListaEmpleados (
+		IPCName (estacion::PATH_NAME, estacion::SEM_EMPLEADOS)
 		, 1, 0666 | IPC_CREAT | IPC_EXCL);
-	Semaphore semSurtidoresLibres(
-		IPCName(estacion::PATH_NAME, estacion::SEM_SURTIDORES_LIBRES)
+	Semaphore semSurtidoresLibres (
+		IPCName (estacion::PATH_NAME, estacion::SEM_SURTIDORES_LIBRES)
 		, 1, 0666 | IPC_CREAT | IPC_EXCL);
+
+	SharedArray<Tarea> areaTareas (
+		IPCName (estacion::PATH_NAME, estacion::AREA_EMPLEADOS)
+		, 0666 | IPC_CREAT | IPC_EXCL, args.empleados ());
+	SharedVariable<float> areaCaja (
+		IPCName (estacion::PATH_NAME, estacion::AREA_CAJA)
+		, 0666 | IPC_CREAT | IPC_EXCL);
 
 	semCaja.set(0, 1);
 	semListaSurtidores.set(0, 1);
 	semListaEmpleados.set(0, 1);
 	semSurtidoresLibres.set(0, args.surtidores ());
+
+	areaCaja.set (0);
 
 	std::cout << "Ingrese un texto y presione ENTER para continuar." << std::endl;
 	
