068d05539413b15fae9a720c305e04330422d667
dnietoc@gmail.com <dnietoc@gmail.com@3b725b2d-4dbd-463f-131a-d5313bb6b835>
2014-05-03 23:20:19 +0000

    Inicialización de LoggerRegistry en empleado.

--- src/empleado/main.cc
+++ src/empleado/main.cc
@@ -1,7 +1,15 @@
 #include <Empleado.h>
+#include <estacion/constantes.h>
+#include <logging/LoggerRegistry.h>
 
 int main (int argc, char **argv)
 {
+	LoggerRegistry& registry = LoggerRegistry::getInstance ();
+	registry.filename (estacion::LOG_FILE);
+	registry.application ("empleado");
+	// TODO: parsear argv y obtener el flag de depuración
+	registry.quiet (false);
+
 	// TODO: parsear argv y obtener el id del empleado
 	Empleado empleado (1);
 	empleado.run ();
