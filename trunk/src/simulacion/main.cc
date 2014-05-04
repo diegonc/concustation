1fb0a2606632e3a2b6dfc716d0af71ec839ce813
dnietoc@gmail.com <dnietoc@gmail.com@3b725b2d-4dbd-463f-131a-d5313bb6b835>
2014-05-04 16:22:49 +0000

    Clase Simulacion para encapsular la inicialización.

--- trunk/src/simulacion/main.cc
+++ trunk/src/simulacion/main.cc
@@ -1,18 +1,11 @@
-#include <iostream>
-#include <string>
-
-#include <estacion/Configuracion.h>
-#include <estacion/constantes.h>
-#include <estacion/ListaEntero.h>
-#include <estacion/Tarea.h>
-#include <logging/Logger.h>
-#include <logging/LoggerRegistry.h>
-#include <system/FifoFilesystemObject.h>
-#include <system/Semaphore.h>
-#include <system/SharedArray.h>
-#include <system/SharedVariable.h>
 
 #include <ArgParser.h>
+#include <Simulacion.h>
+#include <estacion/constantes.h>
+#include <iostream>
+#include <logging/Logger.h>
+#include <logging/LoggerRegistry.h>
+#include <string>
 
 int main(int argc, char** argv)
 {
@@ -31,61 +24,7 @@ int main(int argc, char** argv)
 	logger << "Empleados: " << args.empleados () << Logger::endl;
 	logger << "Surtidores: " << args.surtidores () << Logger::endl;
 
-	Semaphore semCaja (
-		IPCName (estacion::PATH_NAME, estacion::SEM_CAJA)
-		, 1, 0666 | IPC_CREAT | IPC_EXCL);
-	Semaphore semListaSurtidores (
-		IPCName (estacion::PATH_NAME, estacion::SEM_SURTIDORES)
-		, 1, 0666 | IPC_CREAT | IPC_EXCL);
-	Semaphore semListaEmpleados (
-		IPCName (estacion::PATH_NAME, estacion::SEM_EMPLEADOS)
-		, 1, 0666 | IPC_CREAT | IPC_EXCL);
-	Semaphore semSurtidoresLibres (
-		IPCName (estacion::PATH_NAME, estacion::SEM_SURTIDORES_LIBRES)
-		, 1, 0666 | IPC_CREAT | IPC_EXCL);
-
-	SharedVariable<Configuracion> areaConfiguracion (
-		IPCName (estacion::PATH_NAME, estacion::AREA_CONFIGURACION)
-		, 0666 | IPC_CREAT | IPC_EXCL);
-	SharedArray<Tarea> areaTareas (
-		IPCName (estacion::PATH_NAME, estacion::AREA_TAREAS)
-		, 0666 | IPC_CREAT | IPC_EXCL, args.empleados ());
-	SharedVariable<float> areaCaja (
-		IPCName (estacion::PATH_NAME, estacion::AREA_CAJA)
-		, 0666 | IPC_CREAT | IPC_EXCL);
-	ListaEntero listaEmpleados (
-		IPCName (estacion::PATH_NAME, estacion::AREA_EMPLEADOS)
-		, 0666 | IPC_CREAT | IPC_EXCL
-		, args.empleados ()
-		, semListaEmpleados);
-	ListaEntero listaSurtidores (
-		IPCName (estacion::PATH_NAME, estacion::AREA_SURTIDORES)
-		, 0666 | IPC_CREAT | IPC_EXCL
-		, args.surtidores ()
-		, semListaSurtidores);
-	FifoFilesystemObject fifo (estacion::FIFO_NAME, 0666);
-
-	semCaja.set(0, 1);
-	semListaSurtidores.set(0, 1);
-	semListaEmpleados.set(0, 1);
-	semSurtidoresLibres.set(0, args.surtidores ());
-
-	areaConfiguracion.get ().empleados = args.empleados ();
-	areaConfiguracion.get ().surtidores = args.surtidores ();
-
-	areaCaja.set (0);
-
-	listaEmpleados.debug ();
-	for (int i=args.empleados (); i > 0; i--) {
-		listaEmpleados.put (i);
-	}
-	listaEmpleados.debug ();
-
-	listaSurtidores.debug ();
-	for (int i=args.surtidores (); i > 0; i--) {
-		listaSurtidores.put (i);
-	}
-	listaSurtidores.debug ();
+	Simulacion simulacion;
 
 	std::cout << "Ingrese un texto y presione ENTER para continuar." << std::endl;
 	
