c4ff02a99d84d3bfab6ae18467e32147f2ddab0f
dnietoc@gmail.com <dnietoc@gmail.com@3b725b2d-4dbd-463f-131a-d5313bb6b835>
2014-05-05 19:18:01 +0000

    Logear error en execvp.

--- src/lib/system/System.cc
+++ src/lib/system/System.cc
@@ -1,5 +1,7 @@
+#include <estacion/constantes.h>
+#include <logging/Logger.h>
+#include <logging/LoggerRegistry.h>
 #include <system/System.h>
-
 #include <unistd.h>
 
 SystemErrorException::SystemErrorException () throw ()
@@ -15,12 +17,19 @@ SystemErrorException::SystemErrorException (int err) throw ()
 pid_t System::spawn (const char *file, char* const argv[])
 {
 	pid_t pid;
+	bool quiet = LoggerRegistry::getInstance ().quiet ();
 
 	pid = fork ();
 	if (pid == 0) {
 		int err = execvp (file, argv);
-		/* TODO: log error */
-		(void) err;
+		SystemErrorException e;
+		LoggerRegistry& r = LoggerRegistry::getInstance ();
+		r.filename (estacion::LOG_FILE);
+		r.application ("spawned");
+		r.quiet (quiet);
+		Logger& logger = LoggerRegistry::getLogger ("System");
+		logger << "Error ejecutando " << file << " : (" << err << ") "
+		       << e.what () << Logger::endl;
 		_exit (1);
 	}
 	return pid;
