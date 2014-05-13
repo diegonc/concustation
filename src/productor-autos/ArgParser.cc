324df71227d88fa1705ce77b3e78a47501c4f830
dnietoc@gmail.com <dnietoc@gmail.com@3b725b2d-4dbd-463f-131a-d5313bb6b835>
2014-05-13 00:43:05 +0000

    Parametro burst en productor-autos para indicar que se debe generar un bloque de autos.

--- src/productor-autos/ArgParser.cc
+++ src/productor-autos/ArgParser.cc
@@ -10,9 +10,20 @@ int parserFunc (int key, char *arg, struct argp_state *state)
 		case 'd':
 			argParser->_debug = true;
 			break;
+		case 'b':
+		{
+			char* endptr;
+			long burstSize = strtol (arg, &endptr, 10);
+			if (*endptr != '\0' || burstSize == 0) {
+				argp_failure (state, 1, 0, "BURST_SIZE must be a number greater than 0");
+			} else {
+				argParser->_burstSize = static_cast<int> (burstSize);
+			}
+			break;
+		}
 		case ARGP_KEY_END:
 			if (state->arg_num > 0) {
-				argp_failure(state, 1, 0, "too many arguments");
+				argp_failure (state, 1, 0, "too many arguments");
 			}
 			break;
 	}
@@ -22,13 +33,14 @@ int parserFunc (int key, char *arg, struct argp_state *state)
 namespace {
 	struct argp_option options[] = {
 		{ "debug", 'd', 0, 0, "Enable debug mode"},
+		{ "burst", 'b', "BURST_SIZE", 0, "Produce BURST_SIZE items and exit"},
 		{ 0 }
 	};
 
 	struct argp optionParser = { options, parserFunc };
 }
 
-ArgParser::ArgParser () : _debug(false)
+ArgParser::ArgParser () : _debug(false), _burstSize(0)
 {
 }
 
